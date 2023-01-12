7f692ba77b424e801197f478a55a2d15c4bd06a0
PawelHeleniak <pawel.heleniak01@gmail.com>
2023-01-12 12:36:15 +0100

    Clear folder structure, added scss to project

---
+++ src/pages/HomePage/components/Header.js
@@ -0,0 +1,18 @@
+export const Header = () => {
+  return (
+    <header>
+      <div className="wrapper">
+        <div className="headerTitle">
+          <p>Wynajem.pl</p>
+        </div>
+        <div className="headerDescription">
+          <p>Znajdź miejsce na kolejny pobyt</p>
+          <p>Odszukaj odpowiednią oferte dla siebie</p>
+        </div>
+        <div className="searchBox">
+          <button>Szukaj oferty</button>
+        </div>
+      </div>
+    </header>
+  )
+}
