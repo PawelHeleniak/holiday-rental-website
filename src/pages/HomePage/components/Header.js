ca102076845eb1760834c82d288d8c635012e886
PawelHeleniak <pawel.heleniak01@gmail.com>
2023-01-12 19:15:59 +0100

    Header section

--- src/pages/HomePage/components/Header.js
+++ src/pages/HomePage/components/Header.js
@@ -1,16 +1,19 @@
 export const Header = () => {
   return (
     <header>
-      <div className="wrapper">
+      <div className="wrapper header">
         <div className="headerTitle">
           <p>Wynajem.pl</p>
         </div>
-        <div className="headerDescription">
-          <p>Znajdź miejsce na kolejny pobyt</p>
-          <p>Odszukaj odpowiednią oferte dla siebie</p>
-        </div>
-        <div className="searchBox">
-          <button>Szukaj oferty</button>
+        <div className="headerBox">
+          <div className="headerDescription">
+            <p>Znajdź miejsce na kolejny pobyt</p>
+            <p>Szukaj ofert dopasowanych do swoich potrzeb</p>
+          </div>
+          <div className="searchButton">
+            <button>Szukaj oferty</button>
+          </div>
+
         </div>
       </div>
     </header>
