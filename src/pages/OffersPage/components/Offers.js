7d7b7e31375c64bda9b41fc8ae1b607c40e84bf1
PawelHeleniak <pawel.heleniak01@gmail.com>
2023-01-29 08:31:35 +0100

    added schema

--- src/pages/OffersPage/components/Offers.js
+++ src/pages/OffersPage/components/Offers.js
@@ -1,80 +1,25 @@
 import React, { useState } from 'react'
 import { Offer } from './Offer'
-
-//img (temporarily)
-import house from '../../../assets/img/offers/house.jpg'
-import hotel from '../../../assets/img/offers/hotel.jpg'
-import apartament from '../../../assets/img/offers/apartament.jpg'
+import { PRODUCTS } from '../../../schemas/Products'
 
 export const Offers = (props) => {
   const { city, place } = props.options
 
-  //temporarily
-  const offers = [
-    {
-      img: house,
-      title: 'Lorem ipsum dolor sit amet1',
-      adress: 'Zakopane',
-      place: 'Domek',
-      price: '47',
-      cityValue: 1,
-      placeValue: 0,
-    },
-    {
-      img: hotel,
-      title: 'Lorem ipsum dolor sit amet1',
-      adress: 'Zakopane',
-      place: 'Hotel',
-      price: '47',
-      cityValue: 1,
-      placeValue: 1,
-    },
-    {
-      img: house,
-      title: 'Lorem ipsum dolor sit amet1',
-      adress: 'Zakopane',
-      place: 'Domek',
-      price: '47',
-      cityValue: 1,
-      placeValue: 0,
-    },
-    {
-      img: hotel,
-      title: 'Lorem ipsum dolor sit amet2',
-      adress: 'Solina',
-      place: 'Hotel',
-      price: '59',
-      cityValue: 4,
-      placeValue: 1,
-
-    },
-    {
-      img: apartament,
-      title: 'Lorem ipsum dolor sit amet3',
-      adress: 'Solina',
-      place: 'Apartament',
-      price: '107',
-      cityValue: 4,
-      placeValue: 2,
-    },
-  ]
-
-  const offersList = () => {
-    let offersList = [...offers]
+  const productsList = () => {
+    let productsList = [...PRODUCTS]
     let filtered
-    filtered = offersList.filter(offer => offer.cityValue === city && offer.placeValue === place)
+    filtered = productsList.filter(product => product.cityValue === city && product.placeValue === place)
 
     return filtered
   }
-  let offer = offersList().map(e => <Offer data={e} />)
-
+  let product = productsList().map(e => <Offer data={e} />)
   return (
     <section>
       <div className="wrapper offers">
         <div className='offersCount'>
-          <p>Liczba ofert: {offer.length}</p>
+          <p>Liczba ofert: {product.length}</p>
         </div>
-        {offer}
+        {product}
       </div>
     </section>
   )
